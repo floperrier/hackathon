4a6c339e789e4ddbbeba8183f4db3d67b83a7132
jeremdh62 <jdhaille@myges.fr>
2023-06-08 13:10:16 +0200

    update teble HR

--- resources/views/livewire/dashboard/hr.blade.php
+++ resources/views/livewire/dashboard/hr.blade.php
@@ -1,23 +1,12 @@
 <div>
     <div>
-        <h2 class="text-2xl font-semibold leading-tight">Users</h2>
+        <h2 class="text-2xl font-semibold leading-tight">Développeur</h2>
     </div>
-    <div class="my-2 flex sm:flex-row flex-col z-40">
+    <div class="my-2 flex sm:flex-row flex-col justify-end">
         <div class="flex flex-row mb-1 sm:mb-0">
-            <div class="flex-none form-control w-full max-w-xs">
-                <label class="label">
-                    <span class="label-text">Pagination</span>
-                </label>
-                <select wire:model="usersPerPage" class="select select-bordered">
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                </select>
-            </div>
-
-            <div class="flex-none dropdown z-0 relative">
+            <div class="flex-none dropdown z-10 w-full relative">
                 <label tabindex="0" class="btn m-1">Langages <x-bxs-down-arrow class="w-3 h-3" /></label>
-                <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabindex="0" class="dropdown-content menu p-2 shadow block bg-base-100 rounded-box w-52" style="overflow:auto; max-height: 250px;">
                     @foreach ($languagesDatas as $option)
                     <div class="flex gap-x-4 mb-2 items-center">
                         <input wire:model="languages" name="languages" value="{{$option}}" type="checkbox" class="checkbox" />
@@ -34,15 +23,19 @@
           <!-- head -->
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Disponible</th>
+              <th>Noms</th>
+              <th>Travail</th>
+              <th>Salaire</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             @foreach ($users as $user)
                 <tr>
+                <th>
+                    <span class="badge badge-xs" @if($user->available == 'available') style="background-color:green;" @elseif ($user->available == "assigned") style="background-color:orange;" @else style="background-color:red;" @endif></span>
+                </th>
                 <td>
                     <div class="flex items-center space-x-3">
                     <div class="avatar">
@@ -61,17 +54,20 @@
                     </div>
                 </td>
                 <td>
-                    Zemlak, Daniel and Leannon
+                   {{ $user->job_title }}
                     <br/>
                     @if($user->languagesRanks->count() == 0)
-                        <span class="badge badge-ghost badge-sm">None</span>
+                        <span class="badge badge-ghost badge-sm">Aucun langage</span>
                     @else
-                        @foreach ($user->languagesRanks as $lang)
-                            <span class="badge badge-ghost badge-sm">{{$lang->language_name}}</span>
+                        @foreach ($user->languagesRanks as $i => $lang)
+                            <span class="badge badge-ghost badge-sm" @if($i > 2) style="display:none;" @endif>{{$lang->language_name}}</span>
+                            @if($i == 2)
+                                <span class="badge badge-ghost badge-sm">...</span>
+                            @endif
                         @endforeach
                     @endif
                 </td>
-                <td>Purple</td>
+                <td>{{ $user->salary }} € </td>
                 <th>
                     <button class="btn btn-ghost btn-xs">details</button>
                 </th>
